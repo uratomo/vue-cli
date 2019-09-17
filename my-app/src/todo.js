63cf82f7ecea3072693c925fde7721795058df94
uratomo <rop.ibuki@gmail.com>
2019-09-17 13:52:01 +0900

    todo basic

---
+++ my-app/src/todo.js
@@ -0,0 +1,49 @@
+import React ,{Component} from 'react';
+
+
+export default class Todo extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      todos:[],
+      name:''
+    }
+  }
+
+  onInput=(e)=>{
+    this.setState({
+      name:e.target.value
+    })
+  }
+
+  addTodo  = ()=>{
+    const{todos,name} =this.state;
+    this.setState({
+      todos:[...todos,name]
+    });
+  }
+
+  removeTodo= (index)=>{
+    const{todos,name}=this.state;
+    this.setState({
+      todos:[...todos.slice(0,index),...todos.slice(index+1)]
+    });
+    console.log(index)
+  }
+
+  render(){
+    const {todos}=this.state;
+    return(
+      <div>
+      <input type="text" onInput={(e)=>{this.onInput(e)}} />
+      <button onClick={(e)=>{this.addTodo(e)}}>登録</button>
+      <ul>
+        {todos.map((todo, index) => <li key={index}>
+        {todo}
+        <button onClick={()=>{this.removeTodo(index)}}>削除</button>
+        </li>)}
+      </ul>
+    </div>
+      )
+  }
+}
